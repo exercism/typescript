ce0afe90cbb6ea7a0d4bc55a30ce314bea9c792d
elena.savelieva <elena.savelieva@arcadia.spb.ru>
2017-09-08 17:31:42 +0300

    Implement acronym exercise

---
+++ exercises/acronym/acronym.example.ts
@@ -0,0 +1,9 @@
+export default class Acronyms {
+  static parse(phrase: string): string {
+    if (typeof phrase !== 'undefined' && phrase !== undefined ) {
+      const match = phrase.match(/[A-Z]+[a-z]*|[a-z]+/g)
+      return !match ? '' : match.reduce((acronym: string, word: string) => acronym += word[0].toUpperCase(), '')
+    }
+    return ''
+  }
+}
---
+++ exercises/acronym/acronym.test.ts
@@ -0,0 +1,27 @@
+import Acronyms from './acronym'
+
+describe('Acronyms are produced from', () => {
+  it('title cased phrases', () => {
+    expect(Acronyms.parse('Portable Network Graphics')).toEqual('PNG')
+  })
+
+  xit('other title cased phrases', () => {
+    expect(Acronyms.parse('Ruby on Rails')).toEqual('ROR')
+  })
+
+  xit('inconsistently cased phrases', () => {
+    expect(Acronyms.parse('HyperText Markup Language')).toEqual('HTML')
+  })
+
+  xit('phrases with punctuation', () => {
+    expect(Acronyms.parse('First In, First Out')).toEqual('FIFO')
+  })
+
+  xit('other phrases with punctuation', () => {
+    expect(Acronyms.parse('PHP: Hypertext Preprocessor')).toEqual('PHP')
+  })
+
+  xit('phrases with punctuation and sentence casing', () => {
+    expect(Acronyms.parse('Complementary metal-oxide semiconductor')).toEqual('CMOS')
+  })
+})
