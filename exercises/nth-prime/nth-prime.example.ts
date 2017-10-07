da1064e8bf0ca96dba2bcff1b18c02c6c48a77b2
BrianDGLS <briandgls@yahoo.com>
2017-10-07 19:28:00 +0100

    added nth-prime exercise

---
+++ exercises/nth-prime/nth-prime.example.ts
@@ -0,0 +1,28 @@
+const isPrime = (n: number): boolean => {
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+const range = (min: number, max: number): number[] => {
+  const result: number[] = []
+  for (let i = min; i < max; i++) {
+    result.push(i)
+  }
+  return result
+}
+
+class Prime {
+  nth(nthPrime: number): number {
+    if (nthPrime === 0) {
+      throw new Error('Prime is not possible')
+    }
+
+    return range(2, 2e+6).filter(isPrime)[nthPrime - 1]
+  }
+}
+
+export default Prime
