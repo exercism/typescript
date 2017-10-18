81450e4feeb9c5784c26d6f423daba6ef66f04ff
Dan Shin <dshin198@gmail.com>
2017-10-17 21:30:02 -0400

    add exercise specific files

---
+++ exercises/robot-simulator/README.md
@@ -0,0 +1,58 @@
+# Robot Simulator
+
+Write a robot simulator.
+
+A robot factory's test facility needs a program to verify robot movements.
+
+The robots have three possible movements:
+
+- turn right
+- turn left
+- advance
+
+Robots are placed on a hypothetical infinite grid, facing a particular
+direction (north, east, south, or west) at a set of {x,y} coordinates,
+e.g., {3,8}, with coordinates increasing to the north and east.
+
+The robot then receives a number of instructions, at which point the
+testing facility verifies the robot's new position, and in which
+direction it is pointing.
+
+- The letter-string "RAALAL" means:
+  - Turn right
+  - Advance twice
+  - Turn left
+  - Advance once
+  - Turn left yet again
+- Say a robot starts at {7, 3} facing north. Then running this stream
+  of instructions should leave it at {9, 4} facing west.
+
+## Setup
+
+Go through the setup instructions for TypeScript to
+install the necessary dependencies:
+
+http://exercism.io/languages/typescript
+
+## Requirements
+
+Install assignment dependencies:
+
+```bash
+$ yarn install
+```
+
+## Making the test suite pass
+
+Execute the tests with:
+
+```bash
+$ yarn test
+```
+
+## Source
+
+Wikipedia [https://en.wikipedia.org/wiki/Caesar_cipher](https://en.wikipedia.org/wiki/Caesar_cipher)
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
---
+++ exercises/robot-simulator/robot-simulator.example.ts
@@ -0,0 +1,80 @@
+export default class Robot {
+  public coordinates: number[];
+  public bearing: string;
+
+  constructor() {
+    this.coordinates = [0, 0];
+    this.bearing = 'north';
+  }
+
+  at(xcoord: number, ycoord: number) {
+    this.coordinates = [xcoord, ycoord];
+  }
+
+  orient(direction: string) {
+    this.bearing = direction;
+    return `The robot is pointed ${direction}`;
+  }
+
+  advance() {
+    if (this.bearing === 'north') {
+      this.coordinates[1] += 1;
+    } else if (this.bearing === 'south') {
+      this.coordinates[1] -= 1;
+    } else if (this.bearing === 'east') {
+      this.coordinates[0] += 1;
+    } else if (this.bearing === 'west') {
+      this.coordinates[0] -= 1;
+    }
+  }
+
+  turnLeft() {
+    if (this.bearing === 'north') {
+      this.orient('west');
+    } else if (this.bearing === 'south') {
+      this.orient('east');
+    } else if (this.bearing === 'east') {
+      this.orient('north');
+    } else if (this.bearing === 'west') {
+      this.orient('south');
+    }
+  }
+
+  turnRight() {
+    if (this.bearing === 'north') {
+      this.orient('east');
+    } else if (this.bearing === 'south') {
+      this.orient('west');
+    } else if (this.bearing === 'east') {
+      this.orient('south');
+    } else if (this.bearing === 'west') {
+      this.orient('north');
+    }
+  }
+
+  instructions(s: string) {
+    return [...s].map((character) => {
+      if (character === 'L') {
+        return 'turnLeft';
+      } else if (character === 'R') {
+        return 'turnRight';
+      } else if (character === 'A') {
+        return 'advance';
+      } else {
+        throw 'Invalid Instruction'
+      }
+    });
+  }
+
+  place(args: any) {
+    this.coordinates = [args.x, args.y];
+    this.bearing = args.direction;
+  }
+
+  evaluate(s: string) {
+    this.instructions(s).forEach((instruction) => {
+      this[instruction]();
+    });
+  }
+
+}
