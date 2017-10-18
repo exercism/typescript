ef22d6fd19f1eda6fc291ac45764a7ccae7436ba
Dan Shin <dshin198@gmail.com>
2017-10-17 20:43:28 -0400

    add test specific files

---
+++ exercises/largest-series-product/largest-series-product.example.ts
@@ -0,0 +1,52 @@
+export default class Series {
+  private numberString: string
+  private digits: number[]
+
+  constructor(numberString: string) {
+    if (numberString.match('[^0-9]')) {
+      throw new Error('Invalid input.');
+    }
+
+    this.numberString = numberString;
+    this.digits = this.getDigits();
+  }
+
+  getDigits() {
+    return [...this.numberString].map(digit => parseInt(digit, 10));
+  }
+
+  largestProduct(size: number) {
+    if (size < 0) {
+      throw new Error('Invalid input.');
+    }
+
+    let product,
+      max = 0;
+    this.slices(size).forEach((slice) => {
+      product = slice.reduce((a, b) => a * b, 1);
+      if (product > max) {
+        max = product;
+      }
+    });
+    return max;
+  }
+
+  slices(sliceSize: number) {
+    const result = [];
+    let slice = [];
+
+    if (sliceSize > this.digits.length) {
+      throw new Error('Slice size is too big.');
+    }
+
+    for (let i = 0; i < this.digits.length - sliceSize + 1; i++) {
+      for (let j = 0; j < sliceSize; j++) {
+        slice.push(this.digits[i + j]);
+      }
+      result.push(slice);
+      slice = [];
+    }
+
+    return result;
+  }
+}
