644e7c3bb53f4de0064b7a73186ac75654833bb2
Nathan Gingrich <njgingrich@users.noreply.github.com>
2017-11-08 16:26:59 -0500

    Implement diamond - port from ECMAScript (#112)
    * add diamond ts files
    
    * add to config.json and make tests use multiline string
    
    * added updated commom files
    
    * rename import
    
    * add entry to config.json
    
    * updated the uuid to make it unique
    
    * disable trailing space check in tslint

---
+++ exercises/diamond/diamond.test.ts
@@ -0,0 +1,35 @@
+import Diamond from './diamond'
+
+/* tslint:disable no-trailing-whitespace*/
+describe('Make diamond function', () => {
+  const diamond = new Diamond()
+
+  test('test letter A', () => {
+    const result = 'A\n'
+    expect(diamond.makeDiamond('A')).toEqual(result)
+  })
+
+  test('test letter C', () => {
+    const result = `  A  
+ B B 
+C   C
+ B B 
+  A  
+`
+    expect(diamond.makeDiamond('C')).toEqual(result)
+  })
+
+  test('test letter E', () => {
+    const result = `    A    
+   B B   
+  C   C  
+ D     D 
+E       E
+ D     D 
+  C   C  
+   B B   
+    A    
+`
+    expect(diamond.makeDiamond('E')).toEqual(result)
+  })
+})
