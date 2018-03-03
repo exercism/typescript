92896a4e00f045c7c9a08d795c4ec5b81722a9f8
CRivasGomez <rivasgomezcristian@gmail.com>
2018-03-03 18:28:59 +0100

    Add exercise Isogram

---
+++ exercises/isogram/isogram.example.ts
@@ -0,0 +1,31 @@
+class Isogram {
+    static alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    static isIsogram(phrase: string): boolean {
+        const appearances = new Set()
+
+        for (const letter of phrase) {
+            if (this.isLetter(letter)) {
+                if (appearances.has(letter.toLowerCase())) {
+                    return false
+                } else {
+                    appearances.add(letter.toLowerCase())
+                }
+            } else {
+                continue
+            }
+        }
+
+        return true
+    }
+
+    private static isLetter(letter: string): boolean {
+        if (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122 || letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+export default Isogram
---
+++ exercises/isogram/isogram.ts
@@ -0,0 +1,7 @@
+class Isogram {
+    static isIsogram( /* Parameters go here */ ) {
+        // Your code here
+    }
+}
+
+export default Isogram
