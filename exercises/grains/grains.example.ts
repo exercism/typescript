c9721940b65330e3c495b8c2dc611f85de8956bf
Cristian Rivas Gómez <rivasgomezcristian@gmail.com>
2018-03-18 20:30:38 +0100

    Add Grains exercise

---
+++ exercises/grains/grains.example.ts
@@ -0,0 +1,21 @@
+class Grains {
+    static square(square: number): number {
+        if (square <= 0 || square >= 65) {
+            return -1
+        }
+
+        return Math.pow(2, square - 1)
+    }
+
+    static total(): number {
+        let total = 0
+
+        for (let i = 1; i <= 64; i++) {
+            total += this.square(i)
+        }
+
+        return total
+    }
+}
+
+export default Grains
