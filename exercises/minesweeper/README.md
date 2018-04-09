e0927dfe901a0e1dc3c2cfef5d3938f2e2a19cc0
Cristian Rivas Gómez <rivasgomezcristian@gmail.com>
2018-04-09 23:38:22 +0200

    Add exercise Minesweeper

---
+++ exercises/minesweeper/README.md
@@ -0,0 +1,55 @@
+# Minesweeper
+
+Add the numbers to a minesweeper board.
+
+Minesweeper is a popular game where the user has to find the mines using
+numeric hints that indicate how many mines are directly adjacent
+(horizontally, vertically, diagonally) to a square.
+
+In this exercise you have to create some code that counts the number of
+mines adjacent to a square and transforms boards like this (where `*`
+indicates a mine):
+
+    +-----+
+    | * * |
+    |  *  |
+    |  *  |
+    |     |
+    +-----+
+
+into this:
+
+    +-----+
+    |1*3*1|
+    |13*31|
+    | 2*2 |
+    | 111 |
+    +-----+
+
+## Setup
+
+Go through the setup instructions for TypeScript to
+install the necessary dependencies:
+
+http://exercism.io/languages/typescript
+
+## Requirements
+
+Install assignment dependencies:
+
+```bash
+$ yarn install
+```
+
+## Making the test suite pass
+
+Execute the tests with:
+
+```bash
+$ yarn test
+```
+
+
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
---
+++ exercises/minesweeper/minesweeper.example.ts
@@ -0,0 +1,58 @@
+const MINE = '*'
+
+const DELTAS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, 1],
+    [0, -1],
+]
+
+class Minesweeper {
+    annotate(rows: string[]) {
+        if (noDataPresent(rows)) {
+            return rows
+        }
+
+        const inputBoard = rows.map((row) => [...row])
+
+        const result = inputBoard.map((row, x) => [...row].map((cell, y) => cellToMineOrCount(cell, inputBoard, x, y)))
+
+        return stringify(result as string[][])
+    }
+}
+
+function cellToMineOrCount(cell: string, inputBoard: string[][], x: number, y: number) {
+    if (cell === MINE) {
+        return MINE
+    }
+    return countAdjacentMines(inputBoard, x, y) || " "
+}
+
+function countAdjacentMines(board: string[][], x: number, y: number) {
+    return DELTAS
+        .filter((d) => adjacentSquareIsOnBoard(board, x, d))
+        .filter((d) => adjacentSquareHasMine(board, x, y, d))
+        .length
+}
+
+function stringify(board: string[][]) {
+    return board.map((row) => row.join(''))
+}
+
+function noDataPresent(rows: string[]) {
+    return rows.length === 0 || rows[0].length === 0
+}
+
+function adjacentSquareIsOnBoard(board: string[][], x: number, d: number[]) {
+    return board[x + d[0]]
+}
+
+function adjacentSquareHasMine(board: string[][], x: number, y: number, d: number[]) {
+    return board[x + d[0]][y + d[1]] === MINE
+}
+
+export default Minesweeper
