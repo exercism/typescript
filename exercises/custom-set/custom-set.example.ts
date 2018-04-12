23c13c77a5d8424bee9ff247f32a94caef281ae8
Cristian Rivas Gómez <rivasgomezcristian@gmail.com>
2018-04-12 23:54:10 +0200

    Make use of generics

--- exercises/custom-set/custom-set.example.ts
+++ exercises/custom-set/custom-set.example.ts
@@ -1,55 +1,75 @@
-export default class CustomSet {
-    data: number[]
+export default class CustomSet<T> {
+    data: Set<T>
 
-    // tslint:disable-next-line: no-any
-    constructor(data: any[] = []) {
-        this.data = []
-        data.forEach((el) => this.add(parseInt(el, 10)))
+    constructor(data: T[] = []) {
+        this.data = new Set<T>()
+        data.forEach((el) => this.add(el))
     }
 
-    add(el: number) {
-        this.data[el] = el
+    add(el: T) {
+        this.data.add(el)
         return this
     }
 
-    delete(el: number) {
-        delete this.data[el]
+    delete(el: T) {
+        this.data.delete(el)
         return this
     }
 
     size() {
-        return Object.keys(this.data).length
+        return this.data.size
     }
 
     empty() {
-        return this.size() === 0
+        return this.data.size === 0
     }
 
-    contains(el: number) {
-        return this.data[el] !== undefined
+    contains(el: T) {
+        return this.data.has(el)
     }
 
-    eql(other: CustomSet) {
-        return this.size() === other.size() && this.difference(other).size() === 0
+    eql(other: CustomSet<T>) {
+        if (this.data.size !== other.data.size) {
+            return false
+        }
+        for (const item of this.data) {
+            if (!other.data.has(item)) {
+                return false
+            }
+        }
+        return true
     }
 
-    difference(other: CustomSet) {
-        return new CustomSet(Object.keys(this.data).filter((el) => other.data[parseInt(el, 10)] === undefined))
+    difference(other: CustomSet<T>) {
+        const result = new CustomSet<T>()
+        for (const item of this.data) {
+            if (!other.data.has(item)) {
+                result.add(item)
+            }
+        }
+        return result
     }
 
-    disjoint(other: CustomSet) {
+    disjoint(other: CustomSet<T>) {
         return this.size() === 0 || this.difference(other).size() === this.size()
     }
 
-    intersection(other: CustomSet) {
+    intersection(other: CustomSet<T>) {
         return this.difference(this.difference(other))
     }
 
-    union(other: CustomSet) {
-        return new CustomSet(this.toList().concat(other.toList()))
+    union(other: CustomSet<T>) {
+        const result = new CustomSet<T>()
+        for (const item of this.data) {
+            result.add(item)
+        }
+        for (const item of other.data) {
+            result.add(item)
+        }
+        return result
     }
 
-    subset(other: CustomSet) {
+    subset(other: CustomSet<T>) {
         return this.eql(this.intersection(other))
     }
 
