4b6b62f8d9a1423228b78a018eb8bb6172a5d399
Wolfwood <mdenheyer@gmail.com>
2018-10-03 22:50:42 +0200

    convert to ts

---
+++ exercises/alphametics/alphametics.test.ts
@@ -0,0 +1,93 @@
+import Alphametics from './alphametics'
+
+describe('Solve the alphametics puzzle', () => {
+  it('puzzle with three letters', () => {
+    const puzzle = 'I + BB == ILL'
+    const expected = {
+      I: 1,
+      B: 9,
+      L: 0,
+    }
+    expect(new Alphametics(puzzle).solve()).toEqual(expected)
+  })
+
+  xit('solution must have unique value for each letter', () => {
+    const puzzle = 'A == B'
+    expect(new Alphametics(puzzle).solve()).toBeUndefined()
+  })
+
+  xit('leading zero solution is invalid', () => {
+    const puzzle = 'ACA + DD == BD'
+    expect(new Alphametics(puzzle).solve()).toBeUndefined()
+  })
+
+  xit('puzzle with four letters', () => {
+    const puzzle = 'AS + A == MOM'
+    const expected = {
+      A: 9,
+      S: 2,
+      M: 1,
+      O: 0,
+    }
+    expect(new Alphametics(puzzle).solve()).toEqual(expected)
+  })
+
+  xit('puzzle with six letters', () => {
+    const puzzle = 'NO + NO + TOO == LATE'
+    const expected = {
+      N: 7,
+      O: 4,
+      T: 9,
+      L: 1,
+      A: 0,
+      E: 2,
+    }
+    expect(new Alphametics(puzzle).solve()).toEqual(expected)
+  })
+
+  xit('puzzle with seven letters', () => {
+    const puzzle = 'HE + SEES + THE == LIGHT'
+    const expected = {
+      E: 4,
+      G: 2,
+      H: 5,
+      I: 0,
+      L: 1,
+      S: 9,
+      T: 7,
+    }
+    expect(new Alphametics(puzzle).solve()).toEqual(expected)
+  })
+
+  xit('puzzle with eight letters', () => {
+    const puzzle = 'SEND + MORE == MONEY'
+    const expected = {
+      S: 9,
+      E: 5,
+      N: 6,
+      D: 7,
+      M: 1,
+      O: 0,
+      R: 8,
+      Y: 2,
+    }
+    expect(new Alphametics(puzzle).solve()).toEqual(expected)
+  })
+
+  xit('puzzle with ten letters', () => {
+    const puzzle = 'AND + A + STRONG + OFFENSE + AS + A + GOOD == DEFENSE'
+    const expected = {
+      A: 5,
+      D: 3,
+      E: 4,
+      F: 7,
+      G: 8,
+      N: 0,
+      O: 2,
+      R: 1,
+      S: 6,
+      T: 9,
+    }
+    expect(new Alphametics(puzzle).solve()).toEqual(expected)
+  })
+})
