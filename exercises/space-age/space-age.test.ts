38bbf5efae37c2596c21ee13d507536516eb3a4e
Texas Toland <48431+texastoland@users.noreply.github.com>
2019-06-06 19:36:12 +0900

    Specify default precision explicitly

--- exercises/space-age/space-age.test.ts
+++ exercises/space-age/space-age.test.ts
@@ -9,48 +9,48 @@ describe('Space Age', () => {
 
   xit('age in earth years', () => {
     const age = new SpaceAge(1000000000)
-    expect(age.onEarth()).toBeCloseTo(31.69)
+    expect(age.onEarth()).toBeCloseTo(31.69, 2)
   })
 
   xit('age in mercury years', () => {
     const age = new SpaceAge(2134835688)
-    expect(age.onEarth()).toBeCloseTo(67.65)
-    expect(age.onMercury()).toBeCloseTo(280.88)
+    expect(age.onEarth()).toBeCloseTo(67.65, 2)
+    expect(age.onMercury()).toBeCloseTo(280.88, 2)
   })
 
   xit('age in venus years', () => {
     const age = new SpaceAge(189839836)
-    expect(age.onEarth()).toBeCloseTo(6.02)
-    expect(age.onVenus()).toBeCloseTo(9.78)
+    expect(age.onEarth()).toBeCloseTo(6.02, 2)
+    expect(age.onVenus()).toBeCloseTo(9.78, 2)
   })
 
   xit('age in mars years', () => {
     const age = new SpaceAge(2329871239)
-    expect(age.onEarth()).toBeCloseTo(73.83)
-    expect(age.onMars()).toBeCloseTo(39.25)
+    expect(age.onEarth()).toBeCloseTo(73.83, 2)
+    expect(age.onMars()).toBeCloseTo(39.25, 2)
   })
 
   xit('age in jupiter years', () => {
     const age = new SpaceAge(901876382)
-    expect(age.onEarth()).toBeCloseTo(28.58)
-    expect(age.onJupiter()).toBeCloseTo(2.41)
+    expect(age.onEarth()).toBeCloseTo(28.58, 2)
+    expect(age.onJupiter()).toBeCloseTo(2.41, 2)
   })
 
   xit('age in saturn years', () => {
     const age = new SpaceAge(3000000000)
-    expect(age.onEarth()).toBeCloseTo(95.06)
-    expect(age.onSaturn()).toBeCloseTo(3.23)
+    expect(age.onEarth()).toBeCloseTo(95.06, 2)
+    expect(age.onSaturn()).toBeCloseTo(3.23, 2)
   })
 
   xit('age in uranus years', () => {
     const age = new SpaceAge(3210123456)
-    expect(age.onEarth()).toBeCloseTo(101.72)
-    expect(age.onUranus()).toBeCloseTo(1.21)
+    expect(age.onEarth()).toBeCloseTo(101.72, 2)
+    expect(age.onUranus()).toBeCloseTo(1.21, 2)
   })
 
   xit('age in neptune year', () => {
     const age = new SpaceAge(8210123456)
-    expect(age.onEarth()).toBeCloseTo(260.16)
-    expect(age.onNeptune()).toBeCloseTo(1.58)
+    expect(age.onEarth()).toBeCloseTo(260.16, 2)
+    expect(age.onNeptune()).toBeCloseTo(1.58, 2)
   })
 })
