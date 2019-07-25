38cca01a028c9b1847697e0382a3678415969fdf
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-07-25 02:28:56 +0200

    Make whitespace resistant

--- exercises/diamond/diamond.test.ts
+++ exercises/diamond/diamond.test.ts
@@ -1,5 +1,17 @@
 import Diamond from './diamond'
 
+function diamondify(parts: TemplateStringsArray): string {
+  return parts[0].trim()  // Remove leading and trailing whitespace
+    .split("\n")          // Consider each row
+    .map((line) => {
+      return line.trim()    // Remove whitespace at start (and end)
+        .replace(/·/g, ' ') // Use spaces instead of ·
+    })
+    .filter(Boolean)      // Remove empty rows (if any)
+    .join("\n")           // Turn back into a single string
+    + "\n"                // Should have a final newline
+}
+
 describe('Make diamond function', () => {
   const diamond = new Diamond()
 
@@ -9,26 +21,28 @@ describe('Make diamond function', () => {
   })
 
   test('test letter C', () => {
-    const result = `  A
- B B
-C   C
- B B
-  A
-`
+    const result = diamondify`
+      ··A··
+      ·B·B·
+      C···C
+      ·B·B·
+      ··A··
+    `
     expect(diamond.makeDiamond('C')).toEqual(result)
   })
 
   test('test letter E', () => {
-    const result = `    A
-   B B
-  C   C
- D     D
-E       E
- D     D
-  C   C
-   B B
-    A
-`
+    const result = diamondify`
+      ····A····
+      ···B·B···
+      ··C···C··
+      ·D·····D·
+      E·······E
+      ·D·····D·
+      ··C···C··
+      ···B·B···
+      ····A····
+    `
     expect(diamond.makeDiamond('E')).toEqual(result)
   })
 })
