42cf3f23d685c2fa18a970ca4ce506278b24613d
rjossy <rjossy@goodgamestudios.com>
2019-10-26 17:52:36 +0200

    Add exercise resistor color
    Address issue #238

---
+++ exercises/resistor-color/resistor-color.example.ts
@@ -0,0 +1,3 @@
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+export const colorCode = (color: string): number => COLORS.indexOf(color)
