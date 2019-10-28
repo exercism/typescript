718e7e23e9562bf9a7a7da3bfa5d1c2b19e46290
Marcus Francis <marcusmagnusson@outlook.com>
2019-10-28 19:56:10 +0100

    Update space-age.ts

--- exercises/space-age/space-age.ts
+++ exercises/space-age/space-age.ts
@@ -1,7 +1,34 @@
 class SpaceAge {
   constructor(/* Parameters go here */) {
+    // Your code goes here
   }
-  // Your code goes here
+
+  // Tip: create a private method to DRY the public methods
+
+  onMercury() {
+    throw new Error("Implement this function");
+  }
+  onVenus() {
+    throw new Error("Implement this function");
+  }
+  onEarth() {
+    throw new Error("Implement this function");
+  }
+  onMars() {
+    throw new Error("Implement this function");
+  }
+  onJupiter() {
+    throw new Error("Implement this function");
+  }
+  onSaturn() {
+    throw new Error("Implement this function");
+  }
+  onUranus() {
+    throw new Error("Implement this function");
+  }
+  onNeptune() {
+    throw new Error("Implement this function");
+  }
 }
 
 export default SpaceAge;
