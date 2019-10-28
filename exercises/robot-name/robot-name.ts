712933b13f95e69d52086dbf6fb0d66b4bdbb519
Marcus Francis <marcusmagnusson@outlook.com>
2019-10-28 20:00:48 +0100

    Update robot-name.ts

--- exercises/robot-name/robot-name.ts
+++ exercises/robot-name/robot-name.ts
@@ -1,7 +1,15 @@
 class RobotName {
-    constructor( /* Parameters go here */ ) {
-    }
+  get name() {
+    throw new Error("Implement this function");
+  }
+
+  constructor(/* Parameters go here */) {
     // Your code here
+  }
+
+  resetName() {
+    throw new Error("Implement this function");
+  }
 }
 
 export default RobotName;
