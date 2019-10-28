815c8b7a357d2c70d7797a30ad271086ac097b8c
Marcus Francis <marcusmagnusson@outlook.com>
2019-10-28 19:39:51 +0100

    Update anagram.ts

--- exercises/anagram/anagram.ts
+++ exercises/anagram/anagram.ts
@@ -1,7 +1,11 @@
 class Anagram {
-  constructor( /* Parameters go here */ ) {
+  constructor(/* Parameters go here */) {
+    // Your code goes here
   }
-  // Your code goes here
+
+  matches() {
+    throw new Error("Implement this function");
+  }
 }
 
 export default Anagram;
