ea902894c3e5e6ccf0b841b4a0e42e2f53527c83
Roshan Jossy <roshanjossey@gmail.com>
2020-12-14 19:26:27 +0100

    Add exercise resistor color (#300)
    * Add exercise resistor color
    
    Address issue #238
    
    * Add entry for resitor color in config.json
    
    * Sync this exercise
    
    Co-authored-by: rjossy <rjossy@goodgamestudios.com>
    Co-authored-by: Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>

---
+++ exercises/resistor-color/resistor-color.test.ts
@@ -0,0 +1,21 @@
+import { colorCode, COLORS } from './resistor-color'
+
+describe('color code', () => {
+  it('Black', () => {
+    expect(colorCode('black')).toEqual(0)
+  })
+
+  xit('White', () => {
+    expect(colorCode('white')).toEqual(9)
+  })
+
+  xit('Orange', () => {
+    expect(colorCode('orange')).toEqual(3)
+  })
+})
+
+describe('Colors', () => {
+  xit('returns all colors', () => {
+    expect(COLORS).toEqual(["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"])
+  })
+})
