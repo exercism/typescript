d11ba8d6498f49524d44609b17eb1ada4e7d716c
Mustafa Kemal Tuna <12192118+lumosmind@users.noreply.github.com>
2020-12-22 20:00:01 +0300

    list is always an array
    Type;Script guarantees that list is an array of type T. So Array.isArray(list) is unnecesarry.

--- exercises/accumulate/accumulate.example.ts
+++ exercises/accumulate/accumulate.example.ts
@@ -1,7 +1,7 @@
 export default <T, O>(list: T[], accumulator: (arg: T) => O): O[] => {
   const out = []
   let idx = -1
-  const end = Array.isArray(list) ? list.length : 0
+  const end = list.length;
 
   while (++idx < end) {
     out.push(accumulator(list[idx]))
