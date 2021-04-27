c48c1ff86bd673e9d5bbcd6bd7d4587060c2ed64
JoshiRaez <joseraezrodriguez@gmail.com>
2021-04-28 01:38:00 +0200

    Added function declaration

---
+++ exercises/practice/knapsack/knapsack.ts
@@ -0,0 +1,14 @@
+type Item = {
+  weight: number
+  value: number
+}
+
+export function maximumValue({
+  maximumWeight,
+  items,
+}: {
+  maximumWeight: number
+  items: Item[]
+}): number {
+    //Implement this function
+}
