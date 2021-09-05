0a52d11aae601f4443ea23ce8a5777137e0dc9b5
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-09-05 02:34:35 +0200

    Fix modified stub

--- exercises/practice/accumulate/accumulate.ts
+++ exercises/practice/accumulate/accumulate.ts
@@ -1,4 +1,3 @@
-export function accumulate(list: number, accumulator: unknown): never {
-  list = ''
+export function accumulate(list: unknown, accumulator: unknown): never {
   throw new Error('Remove this statement and implement this function')
 }
