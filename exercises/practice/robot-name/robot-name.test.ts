0e2558848129b1589b64a0dab8195a3134e5b411
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2022-02-13 16:54:12 +0100

    Skip the optional test (#947)

--- exercises/practice/robot-name/robot-name.test.ts
+++ exercises/practice/robot-name/robot-name.test.ts
@@ -125,7 +125,10 @@ describe('Robot', () => {
   })
 
   // This test is optional.
-  xit('all the names can be generated', () => {
+  //
+  // This test doesn't run on our online test runner because it will time-out
+  // with most implementations. It's up to you to test your solution locally.
+  it.skip('all the names can be generated', () => {
     const usedNames = new Set()
     usedNames.add(robot.name)
 
