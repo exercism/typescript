26dfae1fe4fda39b71083b7bf08c3f75c09c00c2
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-02-10 11:19:03 +0100

    Sync luhn

--- exercises/practice/luhn/luhn.test.ts
+++ exercises/practice/luhn/luhn.test.ts
@@ -13,6 +13,10 @@ describe('Luhn', () => {
     expect(valid('059')).toBeTruthy()
   })
 
+  xit('a simple valid SIN that becomes invalid if reversed', () => {
+    expect(valid('59')).toBeTruthy()
+  })
+
   xit('a valid Canadian SIN', () => {
     expect(valid('055 444 285')).toBeTruthy()
   })
@@ -25,8 +29,24 @@ describe('Luhn', () => {
     expect(valid('8273 1232 7352 0569')).toBeFalsy()
   })
 
-  xit('valid strings with a non-digit included become invalid', () => {
-    expect(valid('055a 444 285')).toBeFalsy()
+  xit('invalid long number with an even remainder', () => {
+    expect(valid('1 2345 6789 1234 5678 9012')).toBeFalsy()
+  })
+
+  xit('invalid long number with a remainder divisible by 5', () => {
+    expect(valid('1 2345 6789 1234 5678 9013')).toBeFalsy()
+  })
+
+  xit('valid number with an even number of digits', () => {
+    expect(valid('095 245 88')).toBeTruthy()
+  })
+
+  xit('valid number with an odd number of spaces', () => {
+    expect(valid('234 567 891 234')).toBeTruthy()
+  })
+
+  xit('valid strings with a non-digit added at the end become invalid', () => {
+    expect(valid('059a')).toBeFalsy()
   })
 
   xit('valid strings with punctuation included become invalid', () => {
@@ -34,14 +54,38 @@ describe('Luhn', () => {
   })
 
   xit('valid strings with symbols included become invalid', () => {
-    expect(valid('055£ 444$ 285')).toBeFalsy()
+    expect(valid('055# 444$ 285')).toBeFalsy()
   })
 
   xit('single zero with space is invalid', () => {
     expect(valid(' 0')).toBeFalsy()
   })
 
+  xit('more than a single zero is valid', () => {
+    expect(valid('0000 0')).toBeTruthy()
+  })
+
   xit('input digit 9 is correctly converted to output digit 9', () => {
     expect(valid('091')).toBeTruthy()
+  })
+
+  xit('very long input is valid', () => {
+    expect(valid('9999999999 9999999999 9999999999 9999999999')).toBeTruthy()
+  })
+
+  xit('valid luhn with an odd number of digits and non zero first digit', () => {
+    expect(valid('109')).toBeTruthy()
+  })
+
+  xit("using ascii value for non-doubled non-digit isn't allowed", () => {
+    expect(valid('055b 444 285')).toBeFalsy()
+  })
+
+  xit("using ascii value for doubled non-digit isn't allowed", () => {
+    expect(valid(':9')).toBeFalsy()
+  })
+
+  xit("non-numeric, non-space char in the middle with a sum that's divisible by 10 isn't allowed", () => {
+    expect(valid('59%59')).toBeFalsy()
   })
 })
