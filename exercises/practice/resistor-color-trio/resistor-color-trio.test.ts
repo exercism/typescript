035d97b6abc6eaa3dc895ea33a1e3a9ac7a26cb8
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-02-18 21:49:35 +0100

    Sync resistor-color, resistor-color-duo, and resistor-color-trio (#1276)

--- exercises/practice/resistor-color-trio/resistor-color-trio.test.ts
+++ exercises/practice/resistor-color-trio/resistor-color-trio.test.ts
@@ -26,4 +26,30 @@ describe('Resistor Colors', () => {
       '470 kiloohms'
     )
   })
+
+  xit('Blue and violet and blue', () => {
+    expect(decodedResistorValue(['blue', 'violet', 'blue'])).toEqual(
+      '67 megaohms'
+    )
+  })
+
+  xit('Minimum possible value', () => {
+    expect(decodedResistorValue(['black', 'black', 'black'])).toEqual('0 ohms')
+  })
+
+  xit('Maximum possible value', () => {
+    expect(decodedResistorValue(['white', 'white', 'white'])).toEqual(
+      '99 gigaohms'
+    )
+  })
+
+  xit('First two colors make an invalid octal number', () => {
+    expect(decodedResistorValue(['black', 'grey', 'black'])).toEqual('8 ohms')
+  })
+
+  xit('Ignore extra colors', () => {
+    expect(decodedResistorValue(['blue', 'green', 'yellow', 'orange'])).toEqual(
+      '650 kiloohms'
+    )
+  })
 })
