bbd5b47f6825ecbea787f117ff135c5878e8e564
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-03-05 15:42:04 +0100

    Copy proof from JS track

--- exercises/practice/two-bucket/.meta/proof.ci.ts
+++ exercises/practice/two-bucket/.meta/proof.ci.ts
@@ -1,106 +1,121 @@
-type Bucket = 'one' | 'two'
+class Bucket {
+  public name: string
+  public readonly size: number
+  public amount: number
+
+  constructor(name: string, size: number) {
+    this.name = name
+    this.size = size
+    this.amount = 0
+  }
+
+  // accessors
+  public get available(): number {
+    return this.size - this.amount
+  }
+  public get isFull(): boolean {
+    return this.amount === this.size
+  }
+  public get isEmpty(): boolean {
+    return this.amount === 0
+  }
+
+  public fill(): void {
+    this.amount = this.size
+  }
+  public empty(): void {
+    this.amount = 0
+  }
+
+  public pourInto(other: Bucket): void {
+    const quantity = Math.min(this.amount, other.available)
+    this.amount -= quantity
+    other.amount += quantity
+  }
+}
+
+const gcd: (a: number, b: number) => number = (a, b) =>
+  b === 0 ? a : gcd(b, a % b)
 
 export class TwoBucket {
-  private readonly starter: Bucket
-  private readonly x: number
-  private readonly y: number
-  private readonly z: number
+  private readonly buckets: Bucket[]
+  private readonly goal: number
+  public goalBucket: string | undefined
+  public otherBucket: number | undefined
 
-  public goalBucket!: Bucket
-  public otherBucket!: number
+  constructor(size1: number, size2: number, goal: number, start: string) {
+    this.goal = goal
+    this.buckets = [new Bucket('one', size1), new Bucket('two', size2)]
 
-  constructor(x: number, y: number, z: number, starter: Bucket) {
-    this.starter = starter
-    this.x = x
-    this.y = y
-    this.z = z
+    if (start === 'two') {
+      this.buckets.reverse()
+    }
   }
 
-  private reachedGoal(measurements: number[]): boolean {
-    if (measurements[0] === this.z || measurements[1] === this.z) {
-      if (measurements[0] === this.z) {
-        this.goalBucket = 'one'
-        this.otherBucket = measurements[1]
+  private get first(): Bucket {
+    return this.buckets[0]
+  }
+  private get second(): Bucket {
+    return this.buckets[1]
+  }
+
+  private validate(): void {
+    if (this.goal > Math.max(this.first.size, this.second.size)) {
+      throw new Error('Goal is bigger than the largest bucket.')
+    }
+
+    if (this.goal % gcd(this.first.size, this.second.size) !== 0) {
+      throw new Error(
+        'Goal must be a multiple of the GCD of the sizes of the two buckets.'
+      )
+    }
+  }
+
+  private moves(): {
+    moves: string[]
+    goalBucket: string
+    otherBucket: number
+  } {
+    this.validate()
+
+    this.first.empty()
+    this.second.empty()
+    let moves = 0
+
+    // fill the start bucket with the first move
+    this.first.fill()
+    moves += 1
+
+    // optimization: if the other bucket is the right size,
+    // fill it immediately with the second move
+    if (this.second.size === this.goal) {
+      this.second.fill()
+      moves += 1
+    }
+
+    /* eslint-disable-next-line no-constant-condition */
+    while (true) {
+      if (this.first.amount === this.goal) {
+        this.goalBucket = this.first.name
+        this.otherBucket = this.second.amount
+        return moves
+      }
+
+      if (this.second.amount === this.goal) {
+        this.goalBucket = this.second.name
+        this.otherBucket = this.first.amount
+        return moves
+      }
+
+      if (this.first.isEmpty) {
+        this.first.fill()
+      } else if (this.second.isFull) {
+        this.second.empty()
       } else {
-        this.goalBucket = 'two'
-        this.otherBucket = measurements[0]
+        this.first.pourInto(this.second)
       }
-      return true
-    }
-    return false
-  }
 
-  private bigFirst(
-    measurements: number[],
-    moveCount: number,
-    prBool: boolean
-  ): number {
-    let j = measurements[0],
-      k = measurements[1]
-    while (!this.reachedGoal(measurements)) {
-      if (k > this.x && j === 0 && moveCount === 0) {
-        j = this.x
-        k = this.y - j
-      } else if (j === this.x) {
-        j = 0
-      } else if ((k > this.x && j !== 0) || (k > this.x && prBool)) {
-        k -= this.x - j
-        j = this.x
-      } else if (k > this.x || j === 0) {
-        j = k
-        k -= j
-      } else if (k === 0) {
-        k = this.y
-      }
-      measurements = [j, k]
-      moveCount++
-      prBool = !prBool
+      moves += 1
     }
-    return moveCount
-  }
-
-  private smallFirst(
-    measurements: number[],
-    moveCount: number,
-    prBool: boolean
-  ): number {
-    let j = measurements[0],
-      k = measurements[1]
-    while (!this.reachedGoal(measurements)) {
-      if (j === this.x && moveCount === 0) {
-        j = 0
-        k = this.x
-      } else if (j === 0) {
-        j = this.x
-      } else if (j === this.x && k < this.y) {
-        const tempK = k
-        k + j > this.y ? (k = this.y) : (k = tempK + j)
-        tempK + j > this.y ? (j -= this.y - tempK) : (j = 0)
-      } else if (k === this.y) {
-        k = 0
-      } else if (k === 0 && j < this.x) {
-        k = j
-        j = 0
-      }
-      measurements = [j, k]
-      moveCount++
-      prBool = !prBool
-    }
-    return moveCount
-  }
-
-  public moves(): number {
-    let j = 0
-    let k = 0 // j will be running val of bucket one, k = running val of bucket two
-    this.starter === 'one' ? (j = this.x) : (k = this.y)
-    const measurements = [j, k]
-    let moveCount = 0
-    const prBool = true // pour / receive boolean - need to pour or receive every other turn
-    if (this.starter === 'one') {
-      moveCount = this.smallFirst(measurements, moveCount, prBool)
-    } else {
-      moveCount = this.bigFirst(measurements, moveCount, prBool)
-    }
-    return moveCount + 1 // accounts for first move made before loop (and moveCount starts at zero before loop)
   }
 }
