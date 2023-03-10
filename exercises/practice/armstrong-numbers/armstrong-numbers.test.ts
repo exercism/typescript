28507338e44d2dc3051343d27f310d5909294e7a
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-03-10 17:20:48 +0100

    Update test descriptions

--- exercises/practice/armstrong-numbers/armstrong-numbers.test.ts
+++ exercises/practice/armstrong-numbers/armstrong-numbers.test.ts
@@ -1,35 +1,35 @@
 import { isArmstrongNumber } from './armstrong-numbers'
 
 describe('Armstrong Numbers', () => {
-  it('Single digit numbers are Armstrong numbers', () => {
+  it('Single-digit numbers are Armstrong numbers', () => {
     expect(isArmstrongNumber(5)).toBeTruthy()
   })
 
-  xit('There are no 2 digit Armstrong numbers', () => {
+  xit('There are no two-digit Armstrong numbers', () => {
     expect(isArmstrongNumber(10)).toBeFalsy()
   })
 
-  xit('Three digit number that is an Armstrong number', () => {
+  xit('Three-digit number that is an Armstrong number', () => {
     expect(isArmstrongNumber(153)).toBeTruthy()
   })
 
-  xit('Three digit number that is not an Armstrong number', () => {
+  xit('Three-digit number that is not an Armstrong number', () => {
     expect(isArmstrongNumber(100)).toBeFalsy()
   })
 
-  xit('Four digit number that is an Armstrong number', () => {
+  xit('Four-digit number that is an Armstrong number', () => {
     expect(isArmstrongNumber(9474)).toBeTruthy()
   })
 
-  xit('Four digit number that is not an Armstrong number', () => {
+  xit('Four-digit number that is not an Armstrong number', () => {
     expect(isArmstrongNumber(9475)).toBeFalsy()
   })
 
-  xit('Seven digit number that is an Armstrong number', () => {
+  xit('Seven-digit number that is an Armstrong number', () => {
     expect(isArmstrongNumber(9926315)).toBeTruthy()
   })
 
-  xit('Seven digit number that is not an Armstrong number', () => {
+  xit('Seven-digit number that is not an Armstrong number', () => {
     expect(isArmstrongNumber(9926314)).toBeFalsy()
   })
 
