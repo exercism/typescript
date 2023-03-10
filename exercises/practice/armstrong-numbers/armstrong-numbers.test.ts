d7dd877104b13e0e57429cf21723001d868ce44f
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-03-10 17:19:51 +0100

    Sync armstrong-numbers

--- exercises/practice/armstrong-numbers/armstrong-numbers.test.ts
+++ exercises/practice/armstrong-numbers/armstrong-numbers.test.ts
@@ -32,4 +32,16 @@ describe('Armstrong Numbers', () => {
   xit('Seven digit number that is not an Armstrong number', () => {
     expect(isArmstrongNumber(9926314)).toBeFalsy()
   })
+
+  xit('Armstrong number containing seven zeroes', () => {
+    expect(
+      isArmstrongNumber(BigInt('186709961001538790100634132976990'))
+    ).toBeTruthy()
+  })
+
+  xit('The largest and last Armstrong number', () => {
+    expect(
+      isArmstrongNumber(BigInt('115132219018763992565095597973971522401'))
+    ).toBeTruthy()
+  })
 })
