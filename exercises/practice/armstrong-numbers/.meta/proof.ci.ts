3cb74e417eb5d4cc8040cc7e30ccceb2843c324d
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-03-20 15:10:02 +0100

    Sync armstrong-numbers (#1323)

--- exercises/practice/armstrong-numbers/.meta/proof.ci.ts
+++ exercises/practice/armstrong-numbers/.meta/proof.ci.ts
@@ -1,7 +1,9 @@
-export function isArmstrongNumber(input: number): boolean {
+export function isArmstrongNumber(input: number | bigint): boolean {
   const digits = String(input).split('')
   const sum = digits.reduce((total, current) => {
-    return total + Math.pow(parseInt(current, 10), digits.length)
-  }, 0)
-  return sum === input
+    return total + BigInt(parseInt(current, 10)) ** BigInt(digits.length)
+  }, BigInt(0))
+
+  // eslint-disable-next-line eqeqeq
+  return sum == input
 }
