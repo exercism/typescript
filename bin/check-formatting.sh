e1662dddefa69324b3899c347606828979a3c41f
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2023-06-07 16:37:35 +0200

    Force no colour output via json

--- bin/check-formatting.sh
+++ bin/check-formatting.sh
@@ -2,21 +2,16 @@
 
 set -uo pipefail
 
-apt-get install ansi2txt -y
-
-yarn info prettier --name-only
-yarn info prettier --name-only | ansi2txt
-
 if [ -z "${EXERCISM_PRETTIER_VERSION:-}" ]; then
   echo "[format] pulling prettier version from yarn.lock using sed"
-  EXERCISM_PRETTIER_VERSION="$(FORCE_COLOR=0 yarn info prettier --name-only | sed -n -e 's/^.* prettier@npm://p')"
+  EXERCISM_PRETTIER_VERSION="$(yarn info prettier --json --name-only | sed -n -e 's/^"prettier@npm://' -e 's/"//p')"
   echo "[format] expected version is now ${EXERCISM_PRETTIER_VERSION:-}"
 fi
 
 if [ -z "${EXERCISM_PRETTIER_VERSION:-}" ]; then
   echo "Version could not be pulled using sed" >&2
   echo "[format] pulling prettier version from yarn.lock using grep"
-  EXERCISM_PRETTIER_VERSION="$(FORCE_COLOR=0 yarn info prettier --name-only | grep -Po '.*\sprettier@npm:\K[^\s]+')"
+  EXERCISM_PRETTIER_VERSION="$(yarn info prettier --json --name-only | grep -Po '"prettier@npm:\K[^"]+')"
   echo "[format] expected version is now ${EXERCISM_PRETTIER_VERSION:-}"
 fi
 
@@ -30,20 +25,20 @@ if [ -z "${EXERCISM_PRETTIER_VERSION:-}" ]; then
   echo "$(yarn -v)"
   echo ""
   echo "This is what yarn why reports:"
-  echo "$ FORCE_COLOR=0 yarn why prettier"
-  echo "$(FORCE_COLOR=0 yarn why prettier)"
+  echo "$ yarn why prettier"
+  echo "$(yarn why prettier)"
   echo ""
   echo "And yarn info reports the following:"
-  echo "$ FORCE_COLOR=0 yarn info prettier --name-only"
-  echo "$(FORCE_COLOR=0 yarn info prettier --name-only)"
+  echo "$ yarn info prettier --name-only"
+  echo "$(yarn info prettier --name-only)"
   echo ""
   echo "This is the version that can be extracted using grep:"
-  echo "$ FORCE_COLOR=0 yarn info prettier --name-only | grep -Po '.*\sprettier@npm:\K[^\s]+'"
-  echo "└─ $(FORCE_COLOR=0 yarn info prettier --name-only | grep -Po '.*\sprettier@npm:\K[^\s]+')"
+  echo "$ yarn info prettier --json --name-only | grep -Po '"prettier@npm:\K[^"]+'"
+  echo "└─ $(yarn info prettier --json --name-only | grep -Po '"prettier@npm:\K[^"]+')"
   echo ""
   echo "This is the version that can be extracted using sed:"
-  echo "$ yarn why prettier | sed -n -e 's/^.* prettier@npm://p'"
-  echo "└─ $(FORCE_COLOR=0 yarn info prettier --name-only | sed -n -e 's/^.* prettier@npm://p')"
+  echo "$ yarn info prettier --json --name-only | sed -n -e 's/^"prettier@npm://' -e 's/"//p'"
+  echo "└─ $(yarn info prettier --json --name-only | sed -n -e 's/^"prettier@npm://' -e 's/"//p')"
   echo ""
   echo "These files are found in the repo root:"
   echo "$(ls -p | grep -v /)"
