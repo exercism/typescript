d7dafb856a4752be7d413cf7d3d39e35cd3d9368
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2023-06-07 15:14:47 +0200

    Attempt to have both grep and sed

--- bin/format.sh
+++ bin/format.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+
 if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
-  echo "Pulling prettier version from package.json"
+  echo "Pulling prettier version from yarn.lock using grep"
   EXERCISM_PRETTIER_VERSION=$(yarn why prettier | grep -Po '.*\sprettier@npm:\K[^\s]+')
+fi
+
+if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
+  echo "Pulling prettier version from yarn.lock using sed"
+  EXERCISM_PRETTIER_VERSION=$(yarn why prettier | sed -n -e 's/^.* prettier@npm://m' -e 's/ (via npm.*)//p')
 fi
 
 if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
@@ -10,13 +16,19 @@ if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
   echo "This script requires the EXERCISM_PRETTIER_VERSION variable to work."
   echo "Please see https://exercism.org/docs/building/markdown/style-guide for guidance."
   echo "---------------------------------------------------"
+  echo "$(yarn -v)"
+  echo ""
   echo "This is what yarn why reports:"
   echo "$ yarn why prettier"
-  echo "└─ $(yarn why prettier)"
+  echo "$(yarn why prettier)"
   echo ""
   echo "This is the version that can be extracted using grep:"
   echo "$ yarn why prettier | grep -Po '.*\sprettier@npm:\K[^\s]+')"
   echo "└─ $(yarn why prettier | grep -Po '.*\sprettier@npm:\K[^\s]+')"
+  echo ""
+  echo "This is the version that can be extracted using sed:"
+  echo "$ yarn why prettier | grep -Po '.*\sprettier@npm:\K[^\s]+')"
+  echo "└─ $(yarn why prettier | sed -n -e 's/^.* prettier@npm://m' -e 's/ (via npm.*)//p')"
   echo ""
   echo "These files are found in the repo root:"
   echo "$(ls -p | grep -v /)"
