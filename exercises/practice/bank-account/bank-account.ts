0281902f96373fad1acd15c43fe18c69cc2ff894
Owen Rees <owenrees82@gmail.com>
2024-01-25 13:56:05 +0100

    bank-account added (#1418)
    * bank-account added
    
    * prettier
    
    * test renamed to it
    
    * revert prettier on docs

---
+++ exercises/practice/bank-account/bank-account.ts
@@ -0,0 +1,36 @@
+//
+// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class ValueError extends Error {
+  constructor() {
+    super('Bank account error')
+  }
+}
+
+export class BankAccount {
+  constructor() {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  open(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  close(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  deposit(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  withdraw(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  get balance(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+}
