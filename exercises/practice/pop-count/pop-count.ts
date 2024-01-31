047aaaf20c6bec77366f8bf962bb60c42fe59ec9
Owen Rees <owenrees82@gmail.com>
2024-01-31 14:12:46 +0100

    Pop count (#1420)
    * pop-count added
    
    * lint and format

---
+++ exercises/practice/pop-count/pop-count.ts
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Pop Count' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const eggCount = (displayValue: unknown): unknown => {
+  throw new Error('Remove this statement and implement this function')
+}
