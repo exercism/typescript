87b43a754a1ce8dc60983dbb79aa4df99bc783a9
David Brownman <beamneocube@gmail.com>
2024-02-01 22:18:52 -0800

    add yacht exercise

---
+++ exercises/practice/yacht/yacht.ts
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Yacht' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const enum Category {
+  ONES,
+  TWOS,
+  THREES,
+  FOURS,
+  FIVES,
+  SIXES,
+  FULL_HOUSE,
+  FOUR_OF_A_KIND,
+  LITTLE_STRAIGHT,
+  BIG_STRAIGHT,
+  CHOICE,
+  YACHT,
+}
+
+export const score = (/* params go here */): number => {
+  throw new Error('Remove this statement and implement this function')
+}
