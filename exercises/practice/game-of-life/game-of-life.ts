3d72d6ce3da11052e6a2e3b83a81a6715d622cf1
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-04-13 09:36:50 -0700

    Add `game-of-life` (#1586)
    * Add `game-of-life`
    
    * Prettier format
    
    * Add .vscode JSON files

---
+++ exercises/practice/game-of-life/game-of-life.ts
@@ -0,0 +1,13 @@
+export class GameOfLife {
+  constructor(matrix: unknown) {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  public tick(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  public state(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+}
