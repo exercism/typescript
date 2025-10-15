2a0feb78fda8c3088f53a54ba3c333621564aa99
Efan Rows <119373192+0x3at@users.noreply.github.com>
2025-10-15 10:24:55 -0500

    Update resistor-color.test.ts
    Enabled disabled tests and added remaining color tests

--- exercises/practice/resistor-color/resistor-color.test.ts
+++ exercises/practice/resistor-color/resistor-color.test.ts
@@ -1,17 +1,44 @@
-import { describe, xdescribe, it, expect, xit } from '@jest/globals'
+import { describe, it, expect } from '@jest/globals'
 import { colorCode, COLORS } from './resistor-color.ts'
 
 describe('color code', () => {
   it('Black', () => {
     expect(colorCode('black')).toEqual(0)
   })
-
-  xit('White', () => {
+  
+  it('White', () => {
     expect(colorCode('white')).toEqual(9)
   })
-
-  xit('Orange', () => {
+  
+  it('Orange', () => {
     expect(colorCode('orange')).toEqual(3)
   })
+  
+  it('Brown', () => {
+    expect(colorCode('brown')).toEqual(1)
+  })
+  
+  it('Red', () => {
+    expect(colorCode('red')).toEqual(2)
+  })
+  
+  it('Yellow', () => {
+    expect(colorCode('yellow')).toEqual(4)
+  })
+  
+  it('Green', () => {
+    expect(colorCode('green')).toEqual(5)
+  })
+  
+  it('Blue', () => {
+    expect(colorCode('blue')).toEqual(6)
+  })
+  
+  it('Violet', () => {
+    expect(colorCode('violet')).toEqual(7)
+  })
+  
+  it('Grey', () => {
+    expect(colorCode('grey')).toEqual(8)
+  })
 })
-
