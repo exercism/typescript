279fc73395bb84307580eff42aefb877a55e1702
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-01-06 17:19:55 -0800

    Reformat `bottle-song` files (#1602)
    * Reformat `bottle-song` files
    
    * yarn sync for just `bottle-song`
    
    * Update package name
    
    * Avoid unsafe spread
    
    * Run prettier again

--- exercises/practice/bottle-song/bottle-song.ts
+++ exercises/practice/bottle-song/bottle-song.ts
@@ -1,4 +1,6 @@
-
-export const recite = (initialBottleCount: unknown, takeDownCount: unknown): unknown => {
-    throw new Error('Remove this statement and implement this function')
+export const recite = (
+  initialBottleCount: unknown,
+  takeDownCount: unknown
+): unknown => {
+  throw new Error('Remove this statement and implement this function')
 }
