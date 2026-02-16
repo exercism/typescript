3b37895d9786e668ba2c30879141b44aef7de556
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-02-15 21:35:49 -0800

    Sync `flatten-array` tests

--- exercises/practice/flatten-array/flatten-array.test.ts
+++ exercises/practice/flatten-array/flatten-array.test.ts
@@ -2,34 +2,61 @@ import { describe, it, expect, xit } from '@jest/globals'
 import { flatten } from './flatten-array.ts'
 
 describe('Flatten Array', () => {
-  it('no nesting', () => {
+  it('empty', () => {
+    const arr = []
+    const expected = []
+    expect(flatten(arr)).toEqual(expected)
+  })
+
+  xit('no nesting', () => {
+    const arr = [0, 1, 2]
     const expected = [0, 1, 2]
-    expect(flatten([0, 1, 2])).toEqual(expected)
+    expect(flatten(arr)).toEqual(expected)
+  })
+
+  xit('flattens a nested array', () => {
+    const arr = [[[]]]
+    const expected = []
+    expect(flatten(arr)).toEqual(expected)
   })
 
   xit('flattens array with just integers present', () => {
+    const arr = [1, [2, 3, 4, 5, 6, 7], 8]
     const expected = [1, 2, 3, 4, 5, 6, 7, 8]
-    expect(flatten([1, [2, 3, 4, 5, 6, 7], 8])).toEqual(expected)
+    expect(flatten(arr)).toEqual(expected)
   })
 
   xit('5 level nesting', () => {
+    const arr = [0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]
     const expected = [0, 2, 2, 3, 8, 100, 4, 50, -2]
-    expect(flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2])).toEqual(expected)
+    expect(flatten(arr)).toEqual(expected)
   })
 
   xit('6 level nesting', () => {
+    const arr = [1, [2, [[3]], [4, [[5]]], 6, 7], 8]
     const expected = [1, 2, 3, 4, 5, 6, 7, 8]
-    expect(flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8])).toEqual(expected)
+    expect(flatten(arr)).toEqual(expected)
   })
 
-  xit('6 level nest list with null values', () => {
+  xit('consecutive null values at the front of the array are omitted from the final result', () => {
+    const arr = [undefined, undefined, 3]
+    const expected = [3]
+    expect(flatten(arr)).toEqual(expected)
+  })
+
+  xit('consecutive null values in the middle of the array are omitted from the final result', () => {
+    const arr = [1, undefined, undefined, 4]
+    const expected = [1, 4]
+    expect(flatten(arr)).toEqual(expected)
+  })
+
+  xit('6 level nest array with null values', () => {
+    const arr = [0, 2, [[2, 3], 8, [[100]], undefined, [[undefined]]], -2]
     const expected = [0, 2, 2, 3, 8, 100, -2]
-    expect(
-      flatten([0, 2, [[2, 3], 8, [[100]], undefined, [[undefined]]], -2])
-    ).toEqual(expected)
+    expect(flatten(arr)).toEqual(expected)
   })
 
-  xit('all values in nested list are null', () => {
+  xit('all values in nested array are null', () => {
     const expected: number[] = []
     expect(
       flatten([
